81a96299df155c934e694aef5b5c565a92ef85b0
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2024-01-21 14:12:19 +0600

    ok

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -78,7 +78,7 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
-import pdf from "../../assets/RMS-Resume.pdf";
+// import pdf from "../../assets/RMS-Resume.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Zoom from "react-reveal/Zoom";
